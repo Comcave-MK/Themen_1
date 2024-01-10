089c1547e365d46e1e3ab1945f865c522f47fa6f
MichaelK1986 <155629475+MichaelK1986@users.noreply.github.com>
2024-01-10 11:56:45 +0000

    05 / Variante mit Date ()

--- src/05_wahrheitswerte.js
+++ src/05_wahrheitswerte.js
@@ -7,9 +7,12 @@ const birthYearJohn = 2000;
 const birthYearMark = 1990;
 
 // Berchnung Alter 
-ageJohn = 2024 - birthYearJohn;
-ageMark = 2024 - birthYearMark;
+let date = new Date(); // Systemfunktion Datum
+let year = date.getFullYear(); // Daten liefern lassen!
+ageJohn = year - birthYearJohn;
+ageMark = year - birthYearMark;
 
 // Ausgabe
 console.log("ageJohn: " + ageJohn);
 console.log("ageMark: " + ageMark);
+console.log("Datum: " + date);
