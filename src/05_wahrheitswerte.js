0875393628f6ccf2a55a29384be4c5cc0c30df07
MichaelK1986 <155629475+MichaelK1986@users.noreply.github.com>
2024-01-11 08:21:54 +0000

    05 / wahrheitswerte + Deklaration

--- src/05_wahrheitswerte.js
+++ src/05_wahrheitswerte.js
@@ -3,7 +3,7 @@
 
 // Deklaration
 let ageJohn, ageMark;
-const birthYearJohn = 2000;
+const birthYearJohn = 1986;
 const birthYearMark = 1990;
 
 // Berchnung Alter 
@@ -16,3 +16,10 @@ ageMark = year - birthYearMark;
 console.log("ageJohn: " + ageJohn);
 console.log("ageMark: " + ageMark);
 console.log("Datum: " + date);
+
+// Deklaration
+let isJohnOlder // Is John older? --> y/n true / false
+isJohnOlder = (ageJohn > ageMark); // Test 
+isJohnEqual = (ageJohn == ageMark);
+console.log("isJohnOlder: " + isJohnOlder);
+console.log("isJohnEqual: " + isJohnEqual);
