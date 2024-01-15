eea43216899a29bb7cf5e3c0c1cddfbca2749b29
MichaelK1986 <155629475+MichaelK1986@users.noreply.github.com>
2024-01-15 11:10:12 +0000

    09 / calculator() ++

--- src/09_funktionen_II.js
+++ src/09_funktionen_II.js
@@ -6,19 +6,49 @@
 
 1. Dateneingabe + -überprüfung :  
 2. Auswahl Rechenart : 
-3. Fkt. Grundrechenarten : 
+3. Fkt. Grundrechenarten : cheack!
 4. Ausgabe in Konsole : check!
 */
 
 const ERROR_STR_DIV = "Division durch 0 ist nicht möglich!";
+const ERROR_STR_GEN = "Irgendwas ging schief!";
+
+// module: calculator | tests:
+// agreement : "+","-","*",":","/"
+output(calculator(3,2,"+"));
+output(calculator(3,2,"-"));
+output(calculator(3,2,"*"));
+output(calculator(3,2,":"));
+output(calculator(3,2,"/"));
+output(calculator(3,0,"/"));
+output(calculator(3,2,"#?!"));
+function calculator(a,b,op) {
+
+	switch (op) {
+		case "+": // add()
+			return add(a,b)
+		case "-": // sub()
+			return substract(a,b)
+		case "*": // mul()
+			return multiply(a,b)
+		case ":": // div()
+		case "/": // div()
+			return divide(a,b)
+		default: // Error
+			return ERROR_STR_GEN;
+	}
+}
+
+
+
 
 // module: division a / b | test:
-output(divide(4,2));
-output(divide(3,2));
-output(divide(3,-2));
-output(divide(0,2));
-output(divide(3,0));
-output(divide(0,0));
+//output(divide(4,2));
+//output(divide(3,2));
+//output(divide(3,-2));
+//output(divide(0,2));
+//output(divide(3,0));
+//output(divide(0,0));
 function divide(a,b) {
 
 	if (b == 0) {
@@ -33,9 +63,6 @@ function divide(a,b) {
 
 }
 
-
-
-
 // module: multiplication a * b |  test:
 // output(multiply(3,2));
 // output(multiply(3,-2));
@@ -43,6 +70,7 @@ function divide(a,b) {
 function multiply(a,b) {
 	return a * b;
 }
+
 // module: substractiona - b | test:
 // output(subtract(3,2));
 // output(subtract(3,-2));
@@ -52,7 +80,6 @@ function substract(a,b) {
 	return a - b;
 }
 
-
 // module: addition a + b |  test:
 
 // output(add(2,2));
@@ -61,7 +88,6 @@ function substract(a,b) {
 function add(a,b) {
 	return a + b;
 }
-
 
 // module: output | test:
 // output("hello");
