38c962c7c6b744ba26eb9d53d0e7d5d6bdfc0f8c
MichaelK1986 <155629475+MichaelK1986@users.noreply.github.com>
2024-01-16 11:38:10 +0000

    11 /  01a. Funktionalität mit Array 1

--- src/11_Arrays_I.js
+++ src/11_Arrays_I.js
@@ -21,7 +21,6 @@ let arr;
  * 
  */
 
-
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
 // output(getSentence("Ich","bin","Max","Mütze"));
@@ -41,13 +40,27 @@ function getSentence(word1,word2,word3,word4,word5,word6) {
     return str;
 }
 
-
-
-
 // Modul: Ausgabe in Konsole : Test
 // output("hi"); 
 function output(outputStr) {
     console.log(outputStr);
 }
 
-
+/*** 01a. Funktionalität mit Array 1 */
+// Kritik | Überlegung
+// word1,word2, .... wordN :: Semantische Struktur
+// arr[0],arr[1] ... arr[n-1] :: Numerische Struktur (0,1,2,3 ... n)
+// --> Transformation semantisches Problem --> numerisches Problem
+output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]));
+function getSentenceArr(arr) {
+        const GAP = " ";
+        const PUNCT = ".";
+        let str = arr[0] + GAP +
+                  arr[1] + GAP +
+                  arr[2] + GAP +
+                  arr[3] + GAP +
+                  arr[4] + GAP +
+                  arr[5] +
+                  PUNCT;
+    return str;
+}
